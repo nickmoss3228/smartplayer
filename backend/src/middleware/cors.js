11bce8c9e924d810e73d61f78767659c886c8314
Nick <mosknik1337@gmail.com>
2025-12-01 13:30:33 +0500

    removed all the local dev links

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -2,6 +2,7 @@ import cors from "cors";
 
 export const corsMiddleware = cors({
   origin: ['https://infinityplayer.xyz',
+    'https://infinityplayer-git-master-nickmoss3228s-projects.vercel.app/',
     'http://localhost:3000',  
     'http://localhost:5173'], // if using Vite],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
