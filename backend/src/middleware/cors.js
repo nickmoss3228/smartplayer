2192632177bb8b9e67e8578c337ebd615522f82e
Nick <mosknik1337@gmail.com>
2025-12-01 13:17:37 +0500

    fixing env variables

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -1,7 +1,9 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ['https://infinityplayer.xyz',
+    'http://localhost:3000',  
+    'http://localhost:5173'], // if using Vite],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
