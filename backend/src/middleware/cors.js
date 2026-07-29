fcf78b51dfc4fd6111592caae149e3db44a7ee66
Nick <mosknik1337@gmail.com>
2026-07-29 14:47:48 +0500

    added local address

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -7,7 +7,7 @@ export const corsMiddleware = (req, res, next) => {
     'http://localhost:8080',
     'http://localhost:5173',
     'http://localhost:8081',
-    'http://127.0.0.1:5173/',
+    'http://127.0.0.1:5173',
   ];
   
   const origin = req.headers.origin;
